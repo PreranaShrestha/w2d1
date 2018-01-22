69361b2c464572c6dadae9eead43fbf7a5f46138
PreranaShrestha <prerana.sh@gmail.com>
2018-01-22 20:33:13 +0000

    require from http-function and printHtml.js

--- printingWithCallbacks.js
+++ printingWithCallbacks.js
@@ -1,23 +1,5 @@
-function getHTML (options, callback) {
-
-  var https = require('https');
-    var responseBody = '';
-    https.get(options, function(res) {
-      res.on("data", function(chunk) {
-        responseBody += chunk;
-      });
-      res.on("end", function() {
-        callback(responseBody);
-      });
-
-    });
-
-}
-
-function printHTML (html) {
-  console.log(html);
-}
-
+var getHTML = require('./http-functions');
+var printHTML = require('./printHTML');
 var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step4.html'
